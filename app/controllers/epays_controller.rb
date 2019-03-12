3b6ea0d9edb9f5eca4eeda64ffffb8cec7ac82db
StephenVNelson <StephenVNelson@gmail.com>
2019-03-12 15:02:19 -0700

    Successful submission of purchase

--- app/controllers/epays_controller.rb
+++ app/controllers/epays_controller.rb
@@ -13,13 +13,31 @@ class EpaysController < ApplicationController
   end
 
   def create
-    params = params[:transactions]
-    # @epay = HTTParty.post('https://sandbox.usaepay.com/api/v2/transactions',
-    #   headers: {
-    #     'Content-Type' => 'application/json',
-    #     'Authorization' => "Basic #{Rails.application.credentials.usaepay[:authkey]}"
-    #   },
-    #   body: {}
-    # )
+    json_params = epay_params.to_json
+    @epay = HTTParty.post('https://sandbox.usaepay.com/api/v2/transactions',
+      headers: {
+        'Content-Type' => 'application/json',
+        'Authorization' => "Basic #{Rails.application.credentials.usaepay[:authkey]}"
+      },
+      body: json_params
+    )
+  end
+
+  private
+
+  def epay_params
+    params.require(:epay).permit(
+      :command,
+      :amount,
+      :invoice,
+      creditcard: [
+        :cardholder,
+        :number,
+        :expiration,
+        :cvc,
+        :avs_street,
+        :avs_zip,
+      ]
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'epays/index'
+  # get 'epays/index'
+  # get 'epays/new'
+
+  resources :epays, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
