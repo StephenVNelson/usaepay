815a2d983a47043fc0484cdef80504323a3729a1
StephenVNelson <StephenVNelson@gmail.com>
2019-03-12 21:23:03 -0700

    added root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :epays, only: [:index, :new, :create]
   get '/select', to: 'epays#select'
   get '/cart', to: 'epays#cart'
+  root to: 'epays#select'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
